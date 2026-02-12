022451f1a4b0c8be97f3d57faa2929764b1382da
vhav-io <238vaibhavsingh@gmail.com>
2026-02-13 02:25:55 +0530

    added method for checking empty space

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -138,6 +138,13 @@ public class Model extends Observable {
      * */
     public static boolean emptySpaceExists(Board b) {
         // TODO: Fill in this function.
+        for(int row = 0; row<b.size(); row+=1){
+            for(int col = 0; col<b.size(); col+=1){
+                if(b.tile(row,col) == null){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
